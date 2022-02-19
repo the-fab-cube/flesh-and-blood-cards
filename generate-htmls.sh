36e5e8b7ceaf7b2649dac350e61356d21b03444c
Tyler Luce <tjluce@mac.com>
2022-02-19 12:19:04 -0500

    Add set CSV with data on the main sets

--- generate-htmls.sh
+++ generate-htmls.sh
@@ -6,4 +6,5 @@ pyenv exec csvtotable csvs/foiling.csv web/foiling.html -d $'\t' -q $'"' -o
 pyenv exec csvtotable csvs/icon.csv web/icon.html -d $'\t' -q $'"' -o
 pyenv exec csvtotable csvs/keyword.csv web/keyword.html -d $'\t' -q $'"' -o
 pyenv exec csvtotable csvs/rarity.csv web/rarity.html -d $'\t' -q $'"' -o
+pyenv exec csvtotable csvs/set.csv web/set.html -d $'\t' -q $'"' -o
 pyenv exec csvtotable csvs/type.csv web/type.html -d $'\t' -q $'"' -o
