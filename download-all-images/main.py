887f40a2adcf3c624709ca85c4c370dbc8024479
Tyler Luce <tjluce@mac.com>
2022-11-04 09:06:35 -0400

    Added high res images for most of the known cards

--- download-all-images/main.py
+++ download-all-images/main.py
@@ -1,11 +1,14 @@
 import csv
+import getopt
 import re
 import requests
 from os import makedirs
 from os.path import exists
 from pathlib import Path
+import sys
 
 images_dir_path = "images/"
+set_id_to_download = None
 
 def download_image_from_url(image_url: str):
     cleaned_up_image_url = image_url.replace("https://storage.googleapis.com/fabmaster/media/images/", "")
@@ -26,6 +29,19 @@ def download_image_from_url(image_url: str):
     with open(file_name, 'wb') as handler:
         handler.write(img_data)
 
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "hs:")
+except getopt.GetoptError:
+    print('main.py -s <set-id>')
+    sys.exit(2)
+for opt, arg in opts:
+    if opt == '-h':
+        print ('main.py -s <set-id>')
+        sys.exit()
+    elif opt in ("-s", "--set-id"):
+        set_id_to_download = arg
+        print("Downloading only", set_id_to_download, "images")
+
 if not exists(images_dir_path):
     print(images_dir_path + " does not exist, creating it")
     makedirs(images_dir_path)
@@ -36,7 +52,9 @@ with path.open(newline='') as csvfile:
     next(reader)
 
     for row in reader:
-        image_urls = [re.split(" – | - ", url.strip())[0].strip() for url in row[36].split(',')]
-        for image_url in image_urls:
-            download_image_from_url(image_url)
+        # indices: 0 is url, 1 is cardid, 2 is edition, 3 is variations
+        image_urls_split = [re.split(" – | - ", url.strip()) for url in row[36].split(',')]
+        for image_url_data in image_urls_split:
+            if len(image_url_data) >= 3 and (set_id_to_download == None or image_url_data[1].find(set_id_to_download) >= 0):
+                download_image_from_url(image_url_data[0])
 
