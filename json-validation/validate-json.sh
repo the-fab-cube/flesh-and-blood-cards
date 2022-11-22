4ba894eedeb90426b197e331aeac8bb57982b3e1
Tyler Luce <tjluce@mac.com>
2022-11-21 23:20:32 -0500

    Add remaining json schemas

--- json-validation/validate-json.sh
+++ json-validation/validate-json.sh
@@ -2,3 +2,11 @@
 
 ./node_modules/pajv/index.js validate -s ../json-schema/artist-schema.json -d ../json/artist.json
 ./node_modules/pajv/index.js validate -s ../json-schema/card_variation-schema.json -d ../json/card_variation.json
+./node_modules/pajv/index.js validate -s ../json-schema/card-schema.json -d ../json/card.json
+./node_modules/pajv/index.js validate -s ../json-schema/edition-schema.json -d ../json/edition.json
+./node_modules/pajv/index.js validate -s ../json-schema/foiling-schema.json -d ../json/foiling.json
+./node_modules/pajv/index.js validate -s ../json-schema/icon-schema.json -d ../json/icon.json
+./node_modules/pajv/index.js validate -s ../json-schema/keyword-schema.json -d ../json/keyword.json
+./node_modules/pajv/index.js validate -s ../json-schema/rarity-schema.json -d ../json/rarity.json
+./node_modules/pajv/index.js validate -s ../json-schema/set-schema.json -d ../json/set.json
+./node_modules/pajv/index.js validate -s ../json-schema/type-schema.json -d ../json/type.json
