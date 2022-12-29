a254d1ae5f81d89295916fbae7ef0fc9a0e4964b
Tyler Luce <tjluce@mac.com>
2022-12-29 13:31:41 -0500

    Add a Variation Unique IDs column to the card.csv and update generate-unique-ids script to also generate those unique IDs

--- helper-scripts/generate-unique-ids/index.js
+++ helper-scripts/generate-unique-ids/index.js
@@ -18,11 +18,90 @@ const csvStreamFinished = function () {
     console.log('Unique ID generation completed!')
 }
 
+var headerRead = false
+
 csvStream.on("data", function(data) {
-    console.log(data[0].trim() == '' ? 'Generating unique ID for ' + data[1] : 'No new unique ID needed for ' + data[1])
-    if (data[0].trim() == '') {
-        data[0] = customNanoId()
+    // Skip header
+    if (!headerRead) {
+        headerRead = true
+        stringifier.write(data)
+        return
     }
+
+    // CSV indexes
+    let uniqueIdIndex = 0
+    let cardIdIndex = 1
+    let variationsIndex = 36
+    let variationUniqueIdsIndex = 37
+
+    // curren card unique ID data
+    var uniqueID = data[uniqueIdIndex]
+    var cardID = data[cardIdIndex]
+
+    // current variation unique IDs data
+    var variations = data[variationsIndex]
+        .split(",")
+        .filter(x => x.trim() !== '' && x !== undefined)
+        .map(x => {
+            var splitVariation = x.trim().split(RegExp(" — | – | - "))
+            var variation = splitVariation[1] + " – " + splitVariation[2]
+
+            if (splitVariation.length > 3) {
+                variation +=  " – " + splitVariation[3]
+            }
+
+            return variation
+        })
+
+    var existingVariationUniqueIDs = {}
+    data[variationUniqueIdsIndex]
+        .split(",")
+        .filter(x => x.trim() !== '' && x !== undefined)
+        .forEach(x => {
+            var splitVariation = x.trim().split(RegExp(" — | – | - "))
+
+            if (splitVariation.length < 3) {
+                return
+            }
+
+            var variationUniqueId = splitVariation[0]
+            var variation = splitVariation[1] + " – " + splitVariation[2]
+
+            if (splitVariation.length > 3) {
+                variation +=  " – " + splitVariation[3]
+            }
+
+            existingVariationUniqueIDs[variation] = variationUniqueId
+        })
+
+    var uniqueIdExists = uniqueID.trim() !== ''
+
+    // generate unique ID for card
+    if (!uniqueIdExists) {
+        console.log('Generating unique ID for card ' + cardID)
+        data[uniqueIdIndex] = customNanoId()
+    } else {
+        console.log('No new unique ID needed for card ' + cardID)
+    }
+
+    // generate unique IDs for variations
+    var variationUniqueIds = variations.map(variation => {
+        var variationUniqueId = existingVariationUniqueIDs[variation]
+        var variationUniqueIdExists = variationUniqueId !== undefined
+
+        if (!variationUniqueIdExists) {
+            console.log('Generating unique ID for variation ' + variation)
+            variationUniqueId = customNanoId()
+        } else {
+            console.log('No new unique ID needed for variation ' + variation)
+        }
+
+        return variationUniqueId + " – " + variation
+    });
+
+    data[variationUniqueIdsIndex] = variationUniqueIds.join(", ")
+
+    // save CSV row
     stringifier.write(data)
 })
 .on('end', () => {
