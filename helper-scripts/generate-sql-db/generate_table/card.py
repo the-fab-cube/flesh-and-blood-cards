ecb1264fe03156a1eba0421a3fbab37922cf67ff
Tyler Luce <tjluce@mac.com>
2022-12-31 13:50:25 -0500

    Update generate-sql-db to use new card schema

--- helper-scripts/generate-sql-db/generate_table/card.py
+++ helper-scripts/generate-sql-db/generate_table/card.py
@@ -27,16 +27,25 @@ def create_table(cur):
             blitz_legal BOOLEAN NOT NULL DEFAULT TRUE,
             cc_legal BOOLEAN NOT NULL DEFAULT TRUE,
             commoner_legal BOOLEAN NOT NULL DEFAULT TRUE,
-            blitz_living_legend TIMESTAMP,
-            cc_living_legend TIMESTAMP,
-            blitz_banned TIMESTAMP,
-            cc_banned TIMESTAMP,
-            upf_banned TIMESTAMP,
-            commoner_banned TIMESTAMP,
+            blitz_living_legend BOOLEAN NOT NULL DEFAULT FALSE,
+            blitz_living_legend_start TIMESTAMP,
+            cc_living_legend BOOLEAN NOT NULL DEFAULT FALSE,
+            cc_living_legend_start TIMESTAMP,
+            blitz_banned BOOLEAN NOT NULL DEFAULT FALSE,
+            blitz_banned_start TIMESTAMP,
+            cc_banned BOOLEAN NOT NULL DEFAULT FALSE,
+            cc_banned_start TIMESTAMP,
+            upf_banned BOOLEAN NOT NULL DEFAULT FALSE,
+            upf_banned_start TIMESTAMP,
+            commoner_banned BOOLEAN NOT NULL DEFAULT FALSE,
+            commoner_banned_start TIMESTAMP,
+            blitz_suspended BOOLEAN NOT NULL DEFAULT FALSE,
             blitz_suspended_start TIMESTAMP,
             blitz_suspended_end VARCHAR(1000),
+            cc_suspended BOOLEAN NOT NULL DEFAULT FALSE,
             cc_suspended_start TIMESTAMP,
             cc_suspended_end VARCHAR(1000),
+            commoner_suspended BOOLEAN NOT NULL DEFAULT FALSE,
             commoner_suspended_start TIMESTAMP,
             commoner_suspended_end VARCHAR(1000),
             PRIMARY KEY(name, pitch)
@@ -66,24 +75,28 @@ def drop_table(cur):
 
 def insert(cur, unique_id, name, pitch, cost, power, defense, health, intelligence, types, card_keywords, abilities_and_effects,
             ability_and_effect_keywords, granted_keywords, functional_text, functional_text_plain, flavor_text, flavor_text_plain, type_text,
-            played_horizontally, blitz_legal, cc_legal, commoner_legal, blitz_living_legend, cc_living_legend, blitz_banned, cc_banned,
-            commoner_banned, upf_banned, blitz_suspended_start, blitz_suspended_end, cc_suspended_start, cc_suspended_end, commoner_suspended_start,
-            commoner_suspended_end):
+            played_horizontally, blitz_legal, cc_legal, commoner_legal, blitz_living_legend, blitz_living_legend_start, cc_living_legend, cc_living_legend_start,
+            blitz_banned, blitz_banned_start, cc_banned, cc_banned_start, commoner_banned, commoner_banned_start, upf_banned, upf_banned_start,
+            blitz_suspended, blitz_suspended_start, blitz_suspended_end, cc_suspended, cc_suspended_start, cc_suspended_end,
+            commoner_suspended, commoner_suspended_start, commoner_suspended_end):
     sql = """INSERT INTO cards(unique_id, name, pitch, cost, power, defense, health, intelligence, types, card_keywords, abilities_and_effects,
             ability_and_effect_keywords, granted_keywords, functional_text, functional_text_plain, flavor_text, flavor_text_plain, type_text,
-            played_horizontally, blitz_legal, cc_legal, commoner_legal, blitz_living_legend, cc_living_legend, blitz_banned, cc_banned,
-            commoner_banned, upf_banned, blitz_suspended_start, blitz_suspended_end, cc_suspended_start, cc_suspended_end, commoner_suspended_start,
-            commoner_suspended_end)
+            played_horizontally, blitz_legal, cc_legal, commoner_legal, blitz_living_legend, blitz_living_legend_start, cc_living_legend, cc_living_legend_start,
+            blitz_banned, blitz_banned_start, cc_banned, cc_banned_start, commoner_banned, commoner_banned_start, upf_banned, upf_banned_start,
+            blitz_suspended, blitz_suspended_start, blitz_suspended_end, cc_suspended, cc_suspended_start, cc_suspended_end,
+            commoner_suspended, commoner_suspended_start, commoner_suspended_end)
             VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,
             %s, %s, %s, %s, %s, %s, %s,
             %s, %s, %s, %s, %s, %s, %s, %s,
-            %s, %s, %s, %s, %s, %s, %s,
-            %s);"""
+            %s, %s, %s, %s, %s, %s, %s, %s,
+            %s, %s, %s, %s, %s, %s,
+            %s, %s, %s);"""
     data = (unique_id, name, pitch, cost, power, defense, health, intelligence, types, card_keywords, abilities_and_effects,
             ability_and_effect_keywords, granted_keywords, functional_text, functional_text_plain, flavor_text, flavor_text_plain, type_text,
-            played_horizontally, blitz_legal, cc_legal, commoner_legal, blitz_living_legend, cc_living_legend, blitz_banned, cc_banned,
-            commoner_banned, upf_banned, blitz_suspended_start, blitz_suspended_end, cc_suspended_start, cc_suspended_end, commoner_suspended_start,
-            commoner_suspended_end)
+            played_horizontally, blitz_legal, cc_legal, commoner_legal, blitz_living_legend, blitz_living_legend_start, cc_living_legend, cc_living_legend_start,
+            blitz_banned, blitz_banned_start, cc_banned, cc_banned_start, commoner_banned, commoner_banned_start, upf_banned, upf_banned_start,
+            blitz_suspended, blitz_suspended_start, blitz_suspended_end, cc_suspended, cc_suspended_start, cc_suspended_end,
+            commoner_suspended, commoner_suspended_start, commoner_suspended_end)
     try:
         print("Inserting {0} - {1} card with unique id {2}...".format(name, pitch, unique_id))
 
@@ -136,22 +149,32 @@ def generate_table(cur):
             cc_legal = card['cc_legal']
             commoner_legal = card['commoner_legal']
             blitz_living_legend = card['blitz_living_legend']
+            blitz_living_legend_start = card.get('blitz_living_legend_start')
             cc_living_legend = card['cc_living_legend']
+            cc_living_legend_start = card.get('cc_living_legend_start')
             blitz_banned = card['blitz_banned']
+            blitz_banned_start = card.get('blitz_banned_start')
             cc_banned = card['cc_banned']
+            cc_banned_start = card.get('cc_banned_start')
             commoner_banned = card['commoner_banned']
+            commoner_banned_start = card.get('commoner_banned_start')
             upf_banned = card['upf_banned']
-            blitz_suspended_start = card['blitz_suspended_start']
-            blitz_suspended_end = card['blitz_suspended_end']
-            cc_suspended_start = card['cc_suspended_start']
-            cc_suspended_end = card['cc_suspended_end']
-            commoner_suspended_start = card['commoner_suspended_start']
-            commoner_suspended_end = card['commoner_suspended_end']
+            upf_banned_start = card.get('upf_banned_start')
+            blitz_suspended = card['blitz_suspended']
+            blitz_suspended_start = card.get('blitz_suspended_start')
+            blitz_suspended_end = card.get('blitz_suspended_end')
+            cc_suspended = card['cc_suspended']
+            cc_suspended_start = card.get('cc_suspended_start')
+            cc_suspended_end = card.get('cc_suspended_end')
+            commoner_suspended = card['commoner_suspended']
+            commoner_suspended_start = card.get('commoner_suspended_start')
+            commoner_suspended_end = card.get('commoner_suspended_end')
 
             insert(cur, unique_id, name, pitch, cost, power, defense, health, intelligence, types, card_keywords, abilities_and_effects,
             ability_and_effect_keywords, granted_keywords, functional_text, functional_text_plain, flavor_text, flavor_text_plain, type_text,
-            played_horizontally, blitz_legal, cc_legal, commoner_legal, blitz_living_legend, cc_living_legend, blitz_banned, cc_banned,
-            commoner_banned, upf_banned, blitz_suspended_start, blitz_suspended_end, cc_suspended_start, cc_suspended_end, commoner_suspended_start,
-            commoner_suspended_end)
+            played_horizontally, blitz_legal, cc_legal, commoner_legal, blitz_living_legend, blitz_living_legend_start, cc_living_legend, cc_living_legend_start,
+            blitz_banned, blitz_banned_start, cc_banned, cc_banned_start, commoner_banned, commoner_banned_start, upf_banned, upf_banned_start,
+            blitz_suspended, blitz_suspended_start, blitz_suspended_end, cc_suspended, cc_suspended_start, cc_suspended_end,
+            commoner_suspended, commoner_suspended_start, commoner_suspended_end)
 
         print("\nSuccessfully filled cards table\n")
