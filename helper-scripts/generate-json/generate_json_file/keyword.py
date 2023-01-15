17ea80437ad6e229d024410e3917108b8cae86a1
Tyler Luce <tjluce@mac.com>
2023-01-15 10:34:43 -0500

    Add Unique ID to english keyword CSV/JSON

--- helper-scripts/generate-json/generate_json_file/keyword.py
+++ helper-scripts/generate-json/generate_json_file/keyword.py
@@ -3,13 +3,13 @@ import json
 from pathlib import Path
 from markdown_patch import unmark
 
-def generate_json_file():
-    print("Generating keyword.json from keyword.csv...")
+def generate_json_file(language):
+    print(f"Generating {language} keyword.json from keyword.csv...")
 
     keyword_array = []
 
-    csvPath = Path(__file__).parent / "../../../csvs/english/keyword.csv"
-    jsonPath = Path(__file__).parent / "../../../json/english/keyword.json"
+    csvPath = Path(__file__).parent / f"../../../csvs/{language}/keyword.csv"
+    jsonPath = Path(__file__).parent / f"../../../json/{language}/keyword.json"
 
     with csvPath.open(newline='') as csvfile:
         reader = csv.reader(csvfile, delimiter='\t', quotechar='"')
@@ -18,8 +18,9 @@ def generate_json_file():
         for row in reader:
             keyword_object = {}
 
-            keyword_object['keyword'] = row[0]
-            keyword_object['description'] = row[1]
+            keyword_object['unique_id'] = row[0]
+            keyword_object['keyword'] = row[1]
+            keyword_object['description'] = row[2]
             keyword_object['description_plain'] = unmark(keyword_object['description'])
 
             keyword_array.append(keyword_object)
@@ -29,4 +30,4 @@ def generate_json_file():
     with jsonPath.open('w', newline='\n', encoding='utf8') as outfile:
         outfile.write(json_object)
 
-    print("Successfully generated keyword.json\n")
+    print(f"Successfully generated {language} keyword.json\n")
--- helper-scripts/generate-unique-ids/index.js
+++ helper-scripts/generate-unique-ids/index.js
@@ -212,6 +212,66 @@ const generateSetUniqueIds = (language, editionsIndex, editionUniqueIdsIndex) =>
     readStream.pipe(csvStream)
 }
 
+// Set an index to null to omit generation for the associated unique ID
+const generateKeywordUniqueIds = (language, uniqueIdIndex, keywordNameIndex) => {
+    const inputCSV = `../../csvs/${language}/keyword.csv`
+    const outputCSV = `./temp-${language}-keyword.csv`
+
+    const readStream = fs.createReadStream(inputCSV)
+    const writeStream = fs.createWriteStream(outputCSV)
+    const csvStream = csv.parse({ delimiter: "\t" })
+    const stringifier = csv.stringify({ delimiter: "\t" });
+
+    const capitalizedLanguage = capitalizeFirstLetter(language)
+
+    const csvStreamFinished = function (keywordIdsAdded) {
+        fs.renameSync(outputCSV, inputCSV)
+        console.log(`Unique ID generation completed for ${capitalizedLanguage} keywords with ${keywordIdsAdded} new keyword IDs!`)
+    }
+
+    var headerRead = false
+    var keywordIdsAdded = 0
+
+    csvStream.on("data", function(data) {
+        // Skip header
+        if (!headerRead) {
+            headerRead = true
+            stringifier.write(data)
+            return
+        }
+
+        // Keyword Unique ID
+        if (uniqueIdIndex !== null && uniqueIdIndex !== undefined && keywordNameIndex !== null && keywordNameIndex !== undefined) {
+            // current keyword unique ID data
+            var uniqueID = data[uniqueIdIndex]
+            var keywordName = data[keywordNameIndex]
+
+            var uniqueIdExists = uniqueID.trim() !== ''
+
+            // generate unique ID for keyword
+            if (!uniqueIdExists) {
+                console.log(`Generating unique ID for ${capitalizedLanguage} keyword ${keywordName}`)
+                keywordIdsAdded += 1
+                data[uniqueIdIndex] = customNanoId()
+            } else {
+                // console.log(`No new unique ID needed for ${capitalizedLanguage} keyword ${keywordName}`)
+            }
+        }
+
+        // save CSV row
+        stringifier.write(data)
+    })
+    .on('end', () => {
+        csvStreamFinished(keywordIdsAdded)
+    })
+    .on("error", function (error) {
+        console.log(error.message)
+    })
+
+    stringifier.pipe(writeStream)
+    readStream.pipe(csvStream)
+}
+
 generateCardUniqueIds("english", 0, 1, 36, 37)
 generateCardUniqueIds("french", null, null, 14, 15)
 generateCardUniqueIds("german", null, null, 14, 15)
@@ -223,3 +283,5 @@ generateSetUniqueIds("french", 2, 3)
 generateSetUniqueIds("german", 2, 3)
 generateSetUniqueIds("italian", 2, 3)
 generateSetUniqueIds("spanish", 2, 3)
+
+generateKeywordUniqueIds("english", 0, 1)
