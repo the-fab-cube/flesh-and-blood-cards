af7a68dff999bcba4abc6e9ecb887e2bd86c8d27
Tyler Luce <tjluce@mac.com>
2023-01-17 00:23:39 -0500

    Fix JSON validation

--- helper-scripts/json-validation/validate-json.sh
+++ helper-scripts/json-validation/validate-json.sh
@@ -12,21 +12,29 @@
 ./node_modules/pajv/index.js validate -s ../../json-schema/type-schema.json -d ../../json/english/type.json
 
 ./node_modules/pajv/index.js validate -s ../../json-schema/artist-schema.json -d ../../json/french/artist.json
-./node_modules/pajv/index.js validate -s ../../json-schema/artist-schema.json -d ../../json/french/card.json
-./node_modules/pajv/index.js validate -s ../../json-schema/artist-schema.json -d ../../json/french/card-flattened.json
-./node_modules/pajv/index.js validate -s ../../json-schema/artist-schema.json -d ../../json/french/set.json
+./node_modules/pajv/index.js validate -s ../../json-schema/card-schema.json -d ../../json/french/card.json
+./node_modules/pajv/index.js validate -s ../../json-schema/card-flattened-schema.json -d ../../json/french/card-flattened.json
+./node_modules/pajv/index.js validate -s ../../json-schema/keyword-schema.json -d ../../json/french/keyword.json
+./node_modules/pajv/index.js validate -s ../../json-schema/set-schema.json -d ../../json/french/set.json
+./node_modules/pajv/index.js validate -s ../../json-schema/type-schema.json -d ../../json/french/type.json
 
 ./node_modules/pajv/index.js validate -s ../../json-schema/artist-schema.json -d ../../json/german/artist.json
-./node_modules/pajv/index.js validate -s ../../json-schema/artist-schema.json -d ../../json/german/card.json
-./node_modules/pajv/index.js validate -s ../../json-schema/artist-schema.json -d ../../json/german/card-flattened.json
-./node_modules/pajv/index.js validate -s ../../json-schema/artist-schema.json -d ../../json/german/set.json
+./node_modules/pajv/index.js validate -s ../../json-schema/card-schema.json -d ../../json/german/card.json
+./node_modules/pajv/index.js validate -s ../../json-schema/card-flattened-schema.json -d ../../json/german/card-flattened.json
+./node_modules/pajv/index.js validate -s ../../json-schema/keyword-schema.json -d ../../json/german/keyword.json
+./node_modules/pajv/index.js validate -s ../../json-schema/set-schema.json -d ../../json/german/set.json
+./node_modules/pajv/index.js validate -s ../../json-schema/type-schema.json -d ../../json/german/type.json
 
 ./node_modules/pajv/index.js validate -s ../../json-schema/artist-schema.json -d ../../json/italian/artist.json
-./node_modules/pajv/index.js validate -s ../../json-schema/artist-schema.json -d ../../json/italian/card.json
-./node_modules/pajv/index.js validate -s ../../json-schema/artist-schema.json -d ../../json/italian/card-flattened.json
-./node_modules/pajv/index.js validate -s ../../json-schema/artist-schema.json -d ../../json/italian/set.json
+./node_modules/pajv/index.js validate -s ../../json-schema/card-schema.json -d ../../json/italian/card.json
+./node_modules/pajv/index.js validate -s ../../json-schema/card-flattened-schema.json -d ../../json/italian/card-flattened.json
+./node_modules/pajv/index.js validate -s ../../json-schema/keyword-schema.json -d ../../json/italian/keyword.json
+./node_modules/pajv/index.js validate -s ../../json-schema/set-schema.json -d ../../json/italian/set.json
+./node_modules/pajv/index.js validate -s ../../json-schema/type-schema.json -d ../../json/italian/type.json
 
 ./node_modules/pajv/index.js validate -s ../../json-schema/artist-schema.json -d ../../json/spanish/artist.json
-./node_modules/pajv/index.js validate -s ../../json-schema/artist-schema.json -d ../../json/spanish/card.json
-./node_modules/pajv/index.js validate -s ../../json-schema/artist-schema.json -d ../../json/spanish/card-flattened.json
-./node_modules/pajv/index.js validate -s ../../json-schema/artist-schema.json -d ../../json/spanish/set.json
+./node_modules/pajv/index.js validate -s ../../json-schema/card-schema.json -d ../../json/spanish/card.json
+./node_modules/pajv/index.js validate -s ../../json-schema/card-flattened-schema.json -d ../../json/spanish/card-flattened.json
+./node_modules/pajv/index.js validate -s ../../json-schema/keyword-schema.json -d ../../json/spanish/keyword.json
+./node_modules/pajv/index.js validate -s ../../json-schema/set-schema.json -d ../../json/spanish/set.json
+./node_modules/pajv/index.js validate -s ../../json-schema/type-schema.json -d ../../json/spanish/type.json
