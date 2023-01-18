7cce8247f8adc40faf20cfeaa2f7effbab2947e2
Tyler Luce <tjluce@mac.com>
2023-01-18 00:23:44 -0500

    Update static HTML page with new CSV/JSON files

--- helper-scripts/generate-csv-htmls/generate.sh
+++ helper-scripts/generate-csv-htmls/generate.sh
@@ -1,12 +1,40 @@
 #!/bin/bash
 
-pyenv exec poetry run csvtotable ../../csvs/english/ability.csv ../../web/csvs/ability.html -d $'\t' -q $'"' -o
-pyenv exec poetry run csvtotable ../../csvs/english/artist.csv ../../web/csvs/artist.html -d $'\t' -q $'"' -o
-pyenv exec poetry run csvtotable ../../csvs/english/card.csv ../../web/csvs/card.html -d $'\t' -q $'"' -o
-pyenv exec poetry run csvtotable ../../csvs/english/edition.csv ../../web/csvs/edition.html -d $'\t' -q $'"' -o
-pyenv exec poetry run csvtotable ../../csvs/english/foiling.csv ../../web/csvs/foiling.html -d $'\t' -q $'"' -o
-pyenv exec poetry run csvtotable ../../csvs/english/icon.csv ../../web/csvs/icon.html -d $'\t' -q $'"' -o
-pyenv exec poetry run csvtotable ../../csvs/english/keyword.csv ../../web/csvs/keyword.html -d $'\t' -q $'"' -o
-pyenv exec poetry run csvtotable ../../csvs/english/rarity.csv ../../web/csvs/rarity.html -d $'\t' -q $'"' -o
-pyenv exec poetry run csvtotable ../../csvs/english/set.csv ../../web/csvs/set.html -d $'\t' -q $'"' -o
-pyenv exec poetry run csvtotable ../../csvs/english/type.csv ../../web/csvs/type.html -d $'\t' -q $'"' -o
+pyenv exec poetry run csvtotable ../../csvs/english/ability.csv ../../web/csvs/english/ability.html -d $'\t' -q $'"' -o
+pyenv exec poetry run csvtotable ../../csvs/english/artist.csv ../../web/csvs/english/artist.html -d $'\t' -q $'"' -o
+pyenv exec poetry run csvtotable ../../csvs/english/card.csv ../../web/csvs/english/card.html -d $'\t' -q $'"' -o
+pyenv exec poetry run csvtotable ../../csvs/english/edition.csv ../../web/csvs/english/edition.html -d $'\t' -q $'"' -o
+pyenv exec poetry run csvtotable ../../csvs/english/foiling.csv ../../web/csvs/english/foiling.html -d $'\t' -q $'"' -o
+pyenv exec poetry run csvtotable ../../csvs/english/icon.csv ../../web/csvs/english/icon.html -d $'\t' -q $'"' -o
+pyenv exec poetry run csvtotable ../../csvs/english/keyword.csv ../../web/csvs/english/keyword.html -d $'\t' -q $'"' -o
+pyenv exec poetry run csvtotable ../../csvs/english/rarity.csv ../../web/csvs/english/rarity.html -d $'\t' -q $'"' -o
+pyenv exec poetry run csvtotable ../../csvs/english/set.csv ../../web/csvs/english/set.html -d $'\t' -q $'"' -o
+pyenv exec poetry run csvtotable ../../csvs/english/type.csv ../../web/csvs/english/type.html -d $'\t' -q $'"' -o
+
+pyenv exec poetry run csvtotable ../../csvs/french/ability.csv ../../web/csvs/french/ability.html -d $'\t' -q $'"' -o
+pyenv exec poetry run csvtotable ../../csvs/french/artist.csv ../../web/csvs/french/artist.html -d $'\t' -q $'"' -o
+pyenv exec poetry run csvtotable ../../csvs/french/card.csv ../../web/csvs/french/card.html -d $'\t' -q $'"' -o
+pyenv exec poetry run csvtotable ../../csvs/french/keyword.csv ../../web/csvs/french/keyword.html -d $'\t' -q $'"' -o
+pyenv exec poetry run csvtotable ../../csvs/french/set.csv ../../web/csvs/french/set.html -d $'\t' -q $'"' -o
+pyenv exec poetry run csvtotable ../../csvs/french/type.csv ../../web/csvs/french/type.html -d $'\t' -q $'"' -o
+
+pyenv exec poetry run csvtotable ../../csvs/german/ability.csv ../../web/csvs/german/ability.html -d $'\t' -q $'"' -o
+pyenv exec poetry run csvtotable ../../csvs/german/artist.csv ../../web/csvs/german/artist.html -d $'\t' -q $'"' -o
+pyenv exec poetry run csvtotable ../../csvs/german/card.csv ../../web/csvs/german/card.html -d $'\t' -q $'"' -o
+pyenv exec poetry run csvtotable ../../csvs/german/keyword.csv ../../web/csvs/german/keyword.html -d $'\t' -q $'"' -o
+pyenv exec poetry run csvtotable ../../csvs/german/set.csv ../../web/csvs/german/set.html -d $'\t' -q $'"' -o
+pyenv exec poetry run csvtotable ../../csvs/german/type.csv ../../web/csvs/german/type.html -d $'\t' -q $'"' -o
+
+pyenv exec poetry run csvtotable ../../csvs/italian/ability.csv ../../web/csvs/italian/ability.html -d $'\t' -q $'"' -o
+pyenv exec poetry run csvtotable ../../csvs/italian/artist.csv ../../web/csvs/italian/artist.html -d $'\t' -q $'"' -o
+pyenv exec poetry run csvtotable ../../csvs/italian/card.csv ../../web/csvs/italian/card.html -d $'\t' -q $'"' -o
+pyenv exec poetry run csvtotable ../../csvs/italian/keyword.csv ../../web/csvs/italian/keyword.html -d $'\t' -q $'"' -o
+pyenv exec poetry run csvtotable ../../csvs/italian/set.csv ../../web/csvs/italian/set.html -d $'\t' -q $'"' -o
+pyenv exec poetry run csvtotable ../../csvs/italian/type.csv ../../web/csvs/italian/type.html -d $'\t' -q $'"' -o
+
+pyenv exec poetry run csvtotable ../../csvs/spanish/ability.csv ../../web/csvs/spanish/ability.html -d $'\t' -q $'"' -o
+pyenv exec poetry run csvtotable ../../csvs/spanish/artist.csv ../../web/csvs/spanish/artist.html -d $'\t' -q $'"' -o
+pyenv exec poetry run csvtotable ../../csvs/spanish/card.csv ../../web/csvs/spanish/card.html -d $'\t' -q $'"' -o
+pyenv exec poetry run csvtotable ../../csvs/spanish/keyword.csv ../../web/csvs/spanish/keyword.html -d $'\t' -q $'"' -o
+pyenv exec poetry run csvtotable ../../csvs/spanish/set.csv ../../web/csvs/spanish/set.html -d $'\t' -q $'"' -o
+pyenv exec poetry run csvtotable ../../csvs/spanish/type.csv ../../web/csvs/spanish/type.html -d $'\t' -q $'"' -o
