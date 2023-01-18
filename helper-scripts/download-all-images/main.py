7f1955cde7bb3c6f8227fd20a0adff4572761b1f
Tyler Luce <tjluce@mac.com>
2023-01-18 00:31:29 -0500

    Update download-all-images to pull from all language data

--- helper-scripts/download-all-images/main.py
+++ helper-scripts/download-all-images/main.py
@@ -10,6 +10,21 @@ import sys
 images_dir_path = "images/"
 set_id_to_download = None
 
+def download_images_from_language_data(language):
+    # Download the images
+    path = Path(__file__).parent / f"../../json/{language}/card.json"
+    with path.open(newline='') as jsonfile:
+        card_array = json.load(jsonfile)
+
+        for card in card_array:
+            for printing in card['printings']:
+                image_url = printing['image_url']
+                set_id = printing['set_id']
+
+                if image_url is not None and (set_id_to_download == None or set_id == set_id_to_download):
+                        download_image_from_url(image_url)
+
+
 def download_image_from_url(image_url: str):
     cleaned_up_image_url = image_url.replace("https://storage.googleapis.com/fabmaster/media/images/", "")
     cleaned_up_image_url = cleaned_up_image_url.replace("https://storage.googleapis.com/fabmaster/cardfaces/", "")
@@ -47,16 +62,9 @@ if not exists(images_dir_path):
     print(images_dir_path + " does not exist, creating it")
     makedirs(images_dir_path)
 
-# Download the images
-path = Path(__file__).parent / "../../json/english/card.json"
-with path.open(newline='') as jsonfile:
-    card_array = json.load(jsonfile)
 
-    for card in card_array:
-        for printing in card['printings']:
-            image_url = printing['image_url']
-            set_id = printing['set_id']
-
-            if image_url is not None and (set_id_to_download == None or set_id == set_id_to_download):
-                    download_image_from_url(image_url)
-
+download_images_from_language_data("english")
+download_images_from_language_data("french")
+download_images_from_language_data("german")
+download_images_from_language_data("italian")
+download_images_from_language_data("spanish")
