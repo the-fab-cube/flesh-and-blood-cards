b9c557ccb97950f6129310abf2d2740c388d5b15
Dan Collins <dcollinsn@gmail.com>
2023-01-23 15:58:09 -0500

    Add script to clean formatting issues in CSV files
    Adds a script which fixes incorrect spacing or delimiters in some of
    the structured fields that make up the card.csv file, including
    Image URLs and Variations. Enforcing a normalized format will allow
    easier parsing and reduce errors downstream. The script can easily
    be extended to validate and clean other fields as well.
    
    This script depends only on the python3 standard library, and should
    be invoked from the root of the repository.

---
+++ scripts/clean_csvs.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+import csv
+import re
+
+
+def clean_hyphen_separated(data):
+    """
+    A very simple cleaner that just splits on whitespace-dash-whitespace, and joins on ' - '.
+    Normalizes extra spacing on either side of the dash, as well as the use of dash characters
+    other than the hyphen.
+    """
+
+    # \p{Pd} is a property which matches any hyphen or dash type of punctuation character. This
+    # isn't available in the python standard library, but if you `pip install regex` and
+    # `import regex as re` you will be able to use that instead of the bracketed character class.
+    fields = re.split(r'\s+[-–—]\s+', data)
+    return ' - '.join(fields)
+
+
+def clean_image_urls(data):
+    """
+    A cleaner for the "Image URLs" field in the card.csv file. This field is comma-separated, and
+    each item of that is then hyphen-separated. This function cleans up extra or missing whitespace
+    after the comma, and calls out to `clean_hyphen_separated` to deal with the dashes and
+    whitespace around them.
+    """
+
+    # First, we split on the commas which separate the data for each printing of the card. They may
+    # have erroneous spaces (or lack thereof).
+    entries = re.split(',\s*', data)
+
+    # Then, each entry is hyphen-separated, but we need to make sure they are hyphens (not dashes of
+    # any kind) and that they have exactly one space on each side.
+    new_entries = []
+    for entry in entries:
+        new_entries.append(clean_hyphen_separated(entry))
+
+    cleaned_data = ', '.join(new_entries)
+    return cleaned_data
+
+
+CLEANERS = {
+    "Image URLs": clean_image_urls,
+    "Variations": clean_hyphen_separated,
+}
+
+def clean_fields(reader, fieldnames):
+    """
+    This function handles a single CSV file, iterating over the rows, passing any fields to the
+    appropriate cleaners, and finally returning the cleaned list of dictionaries.
+    """
+
+    cleaned_data = []
+    for row in reader:
+        for key, cleaner_callable in CLEANERS.items():
+            if key in row:
+                row[key] = cleaner_callable(row[key])
+        cleaned_data.append(row)
+    return cleaned_data
+
+def process_file(filename):
+    """
+    This function handles the file I/O for a single CSV file, and passes the data to `clean_fields`
+    to be cleaned.
+    """
+
+    with open(filename, newline='') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter="\t")
+        fieldnames = reader.fieldnames
+        cleaned_data = clean_fields(reader, fieldnames)
+    with open(filename, 'w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter="\t", lineterminator="\n")
+        writer.writeheader()
+        for row in cleaned_data:
+            writer.writerow(row)
+
+
+# Add any additional files to be cleaned here.
+process_file('csvs/card.csv')
