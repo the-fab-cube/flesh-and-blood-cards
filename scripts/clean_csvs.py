6344806667e666b685449bd1d05914d6235d78af
Dan Collins <dcollinsn@gmail.com>
2023-01-24 10:43:20 -0500

    Cleaner: Also clean identifier column, strip whitespace
    This fixes an issue where there's a leading space in the
    "Identifier" column of KSU015, and if the consumer forgets to
    .strip() they will get " KSU015".

--- scripts/clean_csvs.py
+++ scripts/clean_csvs.py
@@ -13,8 +13,19 @@ def clean_hyphen_separated(data):
     # \p{Pd} is a property which matches any hyphen or dash type of punctuation character. This
     # isn't available in the python standard library, but if you `pip install regex` and
     # `import regex as re` you will be able to use that instead of the bracketed character class.
+    data = data.strip()
     fields = re.split(r'\s+[-–—]\s+', data)
     return ' - '.join(fields)
+
+
+def clean_comma_separated(data):
+    """
+    Another very simple cleaner that just splits on comma-optionalwhitespace.
+    """
+
+    data = data.strip()
+    fields = re.split(r',\s*', data)
+    return ', '.join(fields)
 
 
 def clean_image_urls(data):
@@ -27,6 +38,7 @@ def clean_image_urls(data):
 
     # First, we split on the commas which separate the data for each printing of the card. They may
     # have erroneous spaces (or lack thereof).
+    data = data.strip()
     entries = re.split(',\s*', data)
 
     # Then, each entry is hyphen-separated, but we need to make sure they are hyphens (not dashes of
@@ -42,6 +54,7 @@ def clean_image_urls(data):
 CLEANERS = {
     "Image URLs": clean_image_urls,
     "Variations": clean_hyphen_separated,
+    "Identifier": clean_comma_separated,
 }
 
 def clean_fields(reader, fieldnames):
