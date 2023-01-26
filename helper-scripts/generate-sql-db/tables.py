c84bfbb2bce27f4f5b1ee5fdf3c16bb4b6aa6fe4
Tyler Luce <tjluce@mac.com>
2023-01-26 00:38:22 -0500

    Fix an error in the drop_tables function

--- helper-scripts/generate-sql-db/tables.py
+++ helper-scripts/generate-sql-db/tables.py
@@ -24,7 +24,6 @@ def create_tables(conn = None):
     generate_table_data.artist.create_table(cur)
     generate_table_data.set.create_table(cur)
     generate_table_data.set_edition.create_table(cur)
-
     generate_table_data.card.create_table(cur)
     generate_table_data.card_printing.create_table(cur)
     generate_table_data.card_translations.create_table(cur)
@@ -47,9 +46,6 @@ def drop_tables(conn = None):
     generate_table_data.ability_translations.drop_table(cur)
     generate_table_data.ability.drop_table(cur)
     generate_table_data.artist.drop_table(cur)
-    generate_table_data.set_edition.drop_table(cur)
-    generate_table_data.set.drop_table(cur)
-
     generate_table_data.card_translations.drop_table(cur)
     generate_table_data.card_printing.drop_table(cur)
     generate_table_data.card.drop_table(cur)
@@ -59,6 +55,8 @@ def drop_tables(conn = None):
     generate_table_data.keyword_translations.drop_table(cur)
     generate_table_data.keyword.drop_table(cur)
     generate_table_data.rarity.drop_table(cur)
+    generate_table_data.set_edition.drop_table(cur)
+    generate_table_data.set.drop_table(cur)
     generate_table_data.type_translations.drop_table(cur)
     generate_table_data.type.drop_table(cur)
 
@@ -71,7 +69,6 @@ def generate_all_table_data(conn = None, url_for_images = None):
         generate_table_data.artist.generate_table_data(cur, language)
         generate_table_data.set.generate_table_data(cur, language)
         generate_table_data.set_edition.generate_table_data(cur, language)
-
         generate_table_data.card_translations.generate_table_data(cur, language)
         generate_table_data.keyword_translations.generate_table_data(cur, language)
         generate_table_data.type_translations.generate_table_data(cur, language)
@@ -83,7 +80,6 @@ def generate_all_table_data(conn = None, url_for_images = None):
     generate_table_data.artist.generate_table_data(cur, "english")
     generate_table_data.set.generate_table_data(cur, "english")
     generate_table_data.set_edition.generate_table_data(cur, "english")
-
     generate_table_data.card.generate_table_data(cur)
     generate_table_data.card_printing.generate_table_data(cur, url_for_images)
     generate_table_data.edition.generate_table_data(cur)
