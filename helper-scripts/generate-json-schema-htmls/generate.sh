3368097fd0c2d504beeea7cd35a5e81258ef144d
Dan Collins <dcollinsn@gmail.com>
2023-02-09 14:02:58 -0500

    Fix additional problems with the helper scripts on a fresh install

--- helper-scripts/generate-json-schema-htmls/generate.sh
+++ helper-scripts/generate-json-schema-htmls/generate.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-pyenv exec poetry run generate-schema-doc --config-file conf.yaml ../../json-schema ../../web/json-schemas
+if type pyenv > /dev/null 2>&1; then
+    CMD="pyenv exec poetry run generate-schema-doc"
+else
+    CMD="generate-schema-doc"
+fi
+
+$CMD --config-file conf.yaml ../../json-schema ../../web/json-schemas
