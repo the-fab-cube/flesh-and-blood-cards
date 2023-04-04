2be37f67c4717a90068e096b173f673590fcc132
Tyler Luce <tjluce@mac.com>
2023-04-03 23:04:22 -0400

    Add initial card-printing.csv

--- helper-scripts/temp-generate-printings/main.py
+++ helper-scripts/temp-generate-printings/main.py
@@ -9,7 +9,7 @@ import helper_functions
 def create_csv_from_card_csv(language):
     print(f"Generating {language} card-printing.csv from {language} card.csv...")
 
-    set_json_path = Path(__file__).parent / "../../../json/english/set.json"
+    set_json_path = Path(__file__).parent / "../../json/english/set.json"
 
     # Compile list of unique printings from the card.csv
     card_printings = list()
@@ -25,6 +25,7 @@ def create_csv_from_card_csv(language):
             card_unique_id = row['Unique ID']
             name = row['Name']
             pitch = row['Pitch']
+            flavor_text = row['Flavor Text']
             ids = helper_functions.convert_to_array(row['Identifiers'])
             set_ids = helper_functions.convert_to_array(row['Set Identifiers'])
             rarities = helper_functions.convert_to_array(row['Rarity'])
@@ -45,9 +46,10 @@ def create_csv_from_card_csv(language):
             for variation_index, variation in enumerate(variations):
                 card_variation = {}
 
-                card_variation['Card Unique ID'] = unique_id
+                card_variation['Card Unique ID'] = card_unique_id
                 card_variation['Card Name'] = name
                 card_variation['Card Pitch'] = pitch
+                card_variation['Flavor Text'] = flavor_text
 
                 variation_split = re.split("— | – | - ", variation.strip())
 
@@ -98,34 +100,40 @@ def create_csv_from_card_csv(language):
                 card_variation['Art Variation'] = alternative_art_type
                 card_variation['Image URL'] = image_url
 
-                for foiling in foilings:
+                for index, foiling in enumerate(foilings):
                     printing_to_add = card_variation.copy()
                     printing_to_add['Foiling'] = foiling
+
+                    if index != 0:
+                        printing_to_add['Unique ID'] = None
 
                     card_printings.append(printing_to_add)
 
     # Output the sorted list of unique artists as a CSV
-    path = Path(__file__).parent / f"../../csvs/{language}/artist.csv"
+    path = Path(__file__).parent / f"../../csvs/{language}/card-printing.csv"
     with path.open('w', newline='') as csvout:
         fieldnames = [
             'Unique ID',
             'Card Unique ID',
+            'Card Name',
+            'Card Pitch',
             'Card ID',
             'Set Unique ID',
             'Set ID',
             'Edition',
-            'Foiling',
             'Rarity',
-            'Artist',
+            'Foiling',
             'Art Variation',
+            'Artist',
+            'Flavor Text',
             'Image URL',
         ]
-        writer = csv.DictWriter(csvout, lineterminator="\n", fieldnames=fieldnames)
+        writer = csv.DictWriter(csvout, delimiter="\t", lineterminator="\n", fieldnames=fieldnames)
 
         # Add title row
         writer.writeheader()
 
-        for card_printing in card_printings:
+        for card_printing in sorted(card_printings, key=lambda item: item['Set ID']):
             writer.writerow(card_printing)
 
     print(f"Successfully generated {language} card-printing.csv")
@@ -133,7 +141,7 @@ def create_csv_from_card_csv(language):
 
 
 create_csv_from_card_csv("english")
-create_csv_from_card_csv("french")
-create_csv_from_card_csv("german")
-create_csv_from_card_csv("italian")
-create_csv_from_card_csv("spanish")
+# create_csv_from_card_csv("french")
+# create_csv_from_card_csv("german")
+# create_csv_from_card_csv("italian")
+# create_csv_from_card_csv("spanish")
