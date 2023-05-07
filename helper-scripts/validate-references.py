d054ee5e83f2c6a741ee4215b4986812474ef3a0
Tyler Luce <tjluce@mac.com>
2023-05-07 18:53:04 -0400

    Work on updating validate-references.py

--- helper-scripts/validate-references.py
+++ helper-scripts/validate-references.py
@@ -31,23 +31,54 @@ with open(set_filename, newline='') as csvfile:
     allowed_sets = {x['Identifier']:x for x in reader}
 
 # Build a list of card unique_ids
-set_filename = 'csvs/english/card.csv'
-with open(set_filename, newline='') as csvfile:
+card_filename = 'csvs/english/card.csv'
+with open(card_filename, newline='') as csvfile:
     reader = csv.DictReader(csvfile, delimiter="\t")
     allowed_card_unique_ids = []
-    allowed_card_variation_unique_ids = []
 
     for row in reader:
         allowed_card_unique_ids.append(row['Unique ID'])
-        variation_unique_ids = [re.split(r'\s+[-–—]\s+', id)[0] for id in re.split(',\s*', row['Variation Unique IDs'])]
-        for id in variation_unique_ids:
-            allowed_card_variation_unique_ids.append(id)
+
+# Build a list of card printing unique_ids
+card_printing_filename = 'csvs/english/card-printing.csv'
+with open(card_printing_filename, newline='') as csvfile:
+    reader = csv.DictReader(csvfile, delimiter="\t")
+    allowed_card_variation_unique_ids = []
+
+    for row in reader:
+        allowed_card_variation_unique_ids.append(row['Unique ID'])
+
+# Scan card legality CSVs for errors
+legality_csv_filenames = [
+    'csvs/english/banned-blitz.csv',
+    'csvs/english/banned-cc.csv',
+    'csvs/english/banned-commoner.csv',
+    'csvs/english/banned-upf.csv',
+    'csvs/english/living-legend-blitz.csv',
+    'csvs/english/living-legend-cc.csv',
+    'csvs/english/suspended-blitz.csv',
+    'csvs/english/suspended-cc.csv',
+    'csvs/english/suspended-commoner.csv',
+]
+
+for legality_filename in legality_csv_filenames:
+    legality_filename = 'csvs/english/banned-blitz.csv'
+    with open(legality_filename, newline='') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter="\t")
+        for row in reader:
+            card_variation_unique_id = row['Card Unique ID']
+            if card_variation_unique_id not in allowed_card_unique_ids:
+                print(f"Warning: unrecognized card variation unique id {card_variation_unique_id} in {legality_filename} entry for "
+                        f"{row['Card Name']} - {row['Card Pitch']}. Check your spelling or add this to {card_filename}.")
+                errors = True
 
 # Scan card.csv for errors
 card_filename = 'csvs/english/card.csv'
 with open(card_filename, newline='') as csvfile:
     reader = csv.DictReader(csvfile, delimiter="\t")
     for row in reader:
+        # TODO: Duplicate Unique ID
+
         card_types = re.split(',\s*', row['Types'])
         for card_type in card_types:
             if card_type not in allowed_types:
@@ -55,57 +86,46 @@ with open(card_filename, newline='') as csvfile:
                       f"{row['Name']}. Check your spelling or add this to {type_filename}.")
                 errors = True
 
-        set_ids = re.split(',\s*', row['Set Identifiers'])
-        for set_id in set_ids:
-            if set_id not in allowed_sets:
-                print(f"Warning: unrecognized set {set_id} in {card_filename} entry for "
-                      f"{row['Name']}. Check your spelling or add this to {set_filename}.")
+# Scan card-printing.csv for errors
+card_printing_filename = 'csvs/english/card-printing.csv'
+with open(card_printing_filename, newline='') as csvfile:
+    reader = csv.DictReader(csvfile, delimiter="\t")
+    for row in reader:
+        # TODO: Duplicate Unique ID
+
+        set_id = row['Set ID']
+        if set_id not in allowed_sets:
+            print(f"Warning: unrecognized set {set_id} in {card_filename} entry for "
+                  f"{row['Name']}. Check your spelling or add this to {set_filename}.")
+            errors = True
+
+        card_id = row['Card ID']
+        set_id_from_card_id = card_id[0:3]
+        if set_id != set_id:
+            print("Warning: card has a card ID for a set that isn't associated with that "
+                  f"card. Found {card_id} for {row['Name']} in {card_filename}. Check "
+                  "this entry for consistency.")
+            errors = True
+
+        this_set = None
+        try:
+            this_set = allowed_sets[set_id]
+        except KeyError:
+            # This should have been caught already, but make sure we fail anyway
+            errors = True
+        if this_set:
+            # Weird check of this_set because I /don't/ want to catch exceptions anymore
+            if not (this_set['Start Card Id'] <= card_id <= this_set['End Card Id']):
+                print(f"Warning: card ID out of range for this set. Found {card_id} in "
+                        f"{card_filename} entry for {row['Name']} but {set_filename} gives "
+                        f"range {this_set['Start Card Id']} - {this_set['End Card Id']}.")
                 errors = True
 
-        card_ids = re.split(',\s*', row['Identifiers'])
-        for card_id in card_ids:
-            set_id = card_id[0:3]
-            if set_id not in set_ids:
-                print("Warning: card has a card ID for a set that isn't associated with that "
-                      f"card. Found {card_id} for {row['Name']} in {card_filename}. Check "
-                      "this entry for consistency.")
-                errors = True
+        # TODO: Edition
+        # TODO: Rarity
+        # TODO: Foiling
+        # TODO: Art Variation
 
-            this_set = None
-            try:
-                this_set = allowed_sets[set_id]
-            except KeyError:
-                # This should have been caught already, but make sure we fail anyway
-                errors = True
-            if this_set:
-                # Weird check of this_set because I /don't/ want to catch exceptions anymore
-                if not (this_set['Start Card Id'] <= card_id <= this_set['End Card Id']):
-                    print(f"Warning: card ID out of range for this set. Found {card_id} in "
-                          f"{card_filename} entry for {row['Name']} but {set_filename} gives "
-                          f"range {this_set['Start Card Id']} - {this_set['End Card Id']}.")
-                    errors = True
-
-        variations = re.split(',\s*', row['Variations'])
-        for variation in variations:
-            var_data = re.split(r'\s+[-–—]\s+', variation)
-            card_id = var_data[1]
-            if card_id not in card_ids:
-                print("Warning: unrecognized card ID found in the Variations field of "
-                      f"{card_filename} for {row['Name']}. Found {card_id} but that isn't in "
-                      "the Identifiers field. Check this entry for consistency.")
-                errors = True
-
-        image_urls = re.split(',\s*', row['Image URLs'])
-        for image_url in image_urls:
-            if len(image_url) == 0:
-                continue
-            img_data = re.split(r'\s+[-–—]\s+', image_url)
-            card_id = img_data[1]
-            if card_id not in card_ids:
-                print("Warning: unrecognized card ID found in the Image URLs field of "
-                      f"{card_filename} for {row['Name']}. Found {card_id} but that isn't in "
-                      "the Identifiers field. Check this entry for consistency.")
-                errors = True
 
 # Scan card-face-association.csv for errors
 card_face_association_filename = 'csvs/english/card-face-association.csv'
