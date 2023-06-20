d50d77084fa9daee255ab94bad5b4b4593da6949
Tyler Luce <tjluce@mac.com>
2023-06-19 21:29:01 -0400

    Fix a bug in the json generator

--- helper-scripts/generate-json/convert_card_printings_to_dict.py
+++ helper-scripts/generate-json/convert_card_printings_to_dict.py
@@ -60,7 +60,7 @@ def convert_card_printings_to_dict(language, card_printing_csv_path, card_face_a
             card_printing['art_variation'] = helper_functions.treat_blank_string_as_none(row['Art Variation'])
             card_printing['flavor_text'] = row['Flavor Text']
             card_printing['flavor_text_plain'] = unmark(card_printing['flavor_text'])
-            card_printing['image_url'] = row['Image URL']
+            card_printing['image_url'] = helper_functions.treat_blank_string_as_none(row['Image URL'])
 
             if tcgplayer_product_id is not None:
                 card_printing['tcgplayer_product_id'] = tcgplayer_product_id
