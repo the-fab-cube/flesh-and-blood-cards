98960f5eaef2677a8a67ae6bde9fc8b04f3962a5
Tyler Luce <tjluce@mac.com>
2023-10-14 15:53:03 -0400

    Add Tales of Aria + Blitz Decks TCGPlayer Product IDs and make some script changes

--- helper-scripts/populate-tcgplayer-product-ids/index.js
+++ helper-scripts/populate-tcgplayer-product-ids/index.js
@@ -1,11 +1,22 @@
 import { fetchGroupProductDetails } from './fetch-group-product-details.js'
 import { populateProductIds } from './populate-product-ids.js'
 
+const extractBearerTokenFromArgs = () => {
+    const bearer = process.argv[2]
+
+    if (!bearer) {
+        console.error('ERROR: Expected a bearer token as 1st argument!');
+        process.exit(1);
+    }
+
+    return bearer
+}
+
 const extractGroupIdFromArgs = () => {
-    const groupId = parseInt(process.argv[2])
+    const groupId = parseInt(process.argv[3])
 
     if (!groupId) {
-        console.error('ERROR: Expected a TCGPlayer groupId as 1st argument!');
+        console.error('ERROR: Expected a TCGPlayer groupId as 2nd argument!');
         process.exit(1);
     }
 
@@ -13,25 +24,14 @@ const extractGroupIdFromArgs = () => {
 }
 
 const extractSetIdFromArgs = () => {
-    const setId = process.argv[3]
+    const setId = process.argv[4]
 
     if (!setId) {
-        console.error('ERROR: Expected a TCGPlayer groupId as 2nd argument!');
+        console.error('ERROR: Expected a TCGPlayer setId as 3rd argument!');
         process.exit(1);
     }
 
     return setId
-}
-
-const extractBearerTokenFromArgs = () => {
-    const bearer = process.argv[4]
-
-    if (!bearer) {
-        console.error('ERROR: Expected a bearer token as 3rd argument!');
-        process.exit(1);
-    }
-
-    return bearer
 }
 
 const groupId = extractGroupIdFromArgs()
