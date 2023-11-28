069daf8219636a2c08c76ef5efd0b0ee730b25af
Tyler Luce <tjluce@mac.com>
2023-11-28 00:17:38 -0500

    Fix error in db upload script

--- helper-scripts/generate-sql-db/generate_table_data/card.py
+++ helper-scripts/generate-sql-db/generate_table_data/card.py
@@ -145,7 +145,7 @@ def upsert_function(cur, cards):
             cur,
             "cards",
             cards,
-            43,
+            45,
             """(unique_id, name, pitch, cost, power, defense, health, intelligence, types, card_keywords, abilities_and_effects,
             ability_and_effect_keywords, granted_keywords, removed_keywords, interacts_with_keywords, functional_text, functional_text_plain, type_text,
             played_horizontally, blitz_legal, cc_legal, commoner_legal, blitz_living_legend, blitz_living_legend_start, cc_living_legend, cc_living_legend_start,
